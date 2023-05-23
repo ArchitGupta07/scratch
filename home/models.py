2cc29b2976a12276c2f9e7ea4787552cdf4d55fb
SilverMINDX01 <gupta.archit01@gmail.com>
2023-05-23 15:39:20 +0530

    push3

--- home/models.py
+++ home/models.py
@@ -2,21 +2,32 @@ from django.db import models
 
 # Create your models here.
 
-class Profiles(models.Model):
-    name = name = models.CharField(max_length=100,null=False)
-    location = models.CharField(max_length=100)
 
-    def __str__(self):
-        return self.name
     
 
 class Projects(models.Model):
     project_name = models.CharField(max_length=100, null=False)
+    project_notes = models.CharField(max_length=500,default='None')
+    comments = models.CharField(max_length=100,default='None')
+    tags = models.CharField(max_length=100,default='None')
+    
+
 
     def __str__(self):
-        return self.name
+        return self.project_name
 class Gallery(models.Model):
-    project_name = models.CharField(max_length=100, null=False)
+    project_name = models.ForeignKey(Projects,max_length=100, null=True,on_delete=models.CASCADE)
 
     def __str__(self):
-        return self.name
+        return self.project_name
+
+class Profiles(models.Model):
+    first_name = models.CharField(max_length=100,null=False)
+    last_name = models.CharField(max_length=100,null=False,default='None')
+    location = models.CharField(max_length=100)
+    friends = models.CharField(max_length=100,null=False,default='None')
+    email = models.EmailField(default='None')
+    
+
+    def __str__(self):
+        return self.first_name+" "+self.last_name

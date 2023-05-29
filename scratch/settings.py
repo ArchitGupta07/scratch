7ba08e95f0dc740a79bacf968cc5eb99900b9757
Archit Gupta <gupta.archit01@gmail.com>
2023-05-29 10:42:08 +0530

    Added

--- scratch/settings.py
+++ scratch/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-t8dm-(72lh1ms*=qdb9%w+ayhqia=zp#$y6ambe_!@4wtz&!0r
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app','scratch.in', '127.0.0.1']
+ALLOWED_HOSTS = ['.vercel.app', '127.0.0.1']
 
 
 # Application definition

f32ccdc78bf20e83f04bcfc9f780f3568181d584
Archit Gupta <gupta.archit01@gmail.com>
2023-05-29 22:51:30 +0530

    Added

--- scratch/settings.py
+++ scratch/settings.py
@@ -77,26 +77,26 @@ WSGI_APPLICATION = 'scratch.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 # if os.environ['ENVIRONMENT'] == "PRODUCTION":
 
 #     DATABASES['default'] = dj_database_url.config(comm_max_age = 600, ssl_require =True)
 # else:
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'scratch',
-        'USER': 'postgres',
-        'PASSWORD': 'archit@archit',
-        'HOST': 'localhost',
-        'PORT': '5432',
-    }
-    }
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'scratch',
+#         'USER': 'postgres',
+#         'PASSWORD': 'archit@archit',
+#         'HOST': 'localhost',
+#         'PORT': '5432',
+#     }
+#     }
 DATABASES['default'] = dj_database_url.config()
     # DATABASES = {
     # 'default': {
